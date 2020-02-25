7765603901ea826742d0c1d13639e5126bd4c5ea
DaiTar <61405606+DaiTar@users.noreply.github.com>
2020-02-25 14:47:22 +0200

    Update app.py
    Updated app.py file.

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 import tkinter as tk
 from tkinter import ttk
 
+root = tk.Tk()
+root.title("Gift app")
+root.geometry("400x400")
+root.resizable(0, 0)
 
-class MainApp(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        self.title("Text box")
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
 
-root = MainApp()
+main_frame = ttk.Frame(root, padding=50)
+main_frame.grid(column=0, row=0, sticky="NSEW")
+
+main_frame.columnconfigure(0, weight=1)
+main_frame.rowconfigure((0, 1, 2), weight=1)
+
+first_button = ttk.Button(main_frame, text="Norų pateikimas")
+first_button.grid(column=0, row=0, sticky="NSEW")
+second_button = ttk.Button(main_frame, text="Dovanų traukimas")
+second_button.grid(column=0, row=1, pady=50, sticky="NSEW")
+third_button = ttk.Button(main_frame, text="Išeiti", command=root.destroy)
+third_button.grid(column=0, row=2, sticky="NSEW")
 
 root.mainloop()
