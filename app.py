e6ec17d46809575e49c4449af3672b534e291e88
DaiTar <61405606+DaiTar@users.noreply.github.com>
2020-03-03 16:51:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,15 +17,16 @@ class GiftApp(tk.Tk):
         container.columnconfigure(0, weight=1)
         container.rowconfigure(0, weight=1)
 
-        start_frame = MainFrame(container, self)
-        start_frame.grid(column=0, row=0, sticky="NSEW", pady=50, padx=50)
+        # start_frame = MainFrame(container, self)
+        # start_frame.grid(column=0, row=0, sticky="NSEW", pady=50, padx=50)
+        input_frame = InputFrame(container)
+        input_frame.grid(column=0, row=0, sticky="NSEW", pady=30, padx=30)
 
 
 class MainFrame(ttk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent)
 
-        self.controller = controller
         self.columnconfigure(0, weight=1)
         self.rowconfigure((0, 1, 2), weight=1)
 
@@ -33,8 +34,20 @@ class MainFrame(ttk.Frame):
         first_button.grid(column=0, row=0, sticky="NSEW")
         second_button = ttk.Button(self, text="Dovanų traukimas")
         second_button.grid(column=0, row=1, pady=50, sticky="NSEW")
-        third_button = ttk.Button(self, text="Išeiti", command=self.controller.destroy)
+        third_button = ttk.Button(self, text="Išeiti", command=controller.destroy)
         third_button.grid(column=0, row=2, sticky="NSEW")
+
+
+class InputFrame(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.name_input = tk.StringVar
+
+        name_label = ttk.Label(self, text="Vardas: ")
+        name_label.grid(column=0, row=0, sticky="W")
+        name_entry = ttk.Entry(self, width=15, textvariable=self.name_input)
+        name_entry.grid(column=0, row=1)
 
 
 root = GiftApp()
